3025bc041bc6e82b336a0b962c9e6fabfcb48766
LUISfelipeSi <luisraphael399@gmail.com>
2025-02-14 14:12:33 -0300

    Corração

---
+++ Models/Materias.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SGE.Models
+{
+    [Table("Materias")]
+    public class Materias
+    {
+        [Column("Id")]
+        [Display(Name = "Código")]
+        public int Id { get; set; }
+
+        [Column("Titulo")]
+        [Display(Name = "Titulo")]
+        public string Nome { get; set; } = string.Empty;
+
+        [ForeignKey("ProfessoresId")]
+        [Display(Name = "Professores")]
+        public int ProfessoresId { get; set; }
+        public Professores? Professores { get; set; }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,7 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using PrimeiroMVC.Models;
+using SGE.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<Contexto>
+   (options => options.UseSqlServer("Data Source=SB-1490625\\SQLSENAI; Initial Catalog = SGE;Integrated Security = True;TrustServerCertificate = True"));
 
 var app = builder.Build();
 
